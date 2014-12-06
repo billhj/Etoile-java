383ef40b6a13f9f025c44aced43fda2e72aa3127
billhj <gabriel.jing.huang@gmail.com>
2014-12-06 22:26:27 +0100

    need to know their parameters

--- OdeApp/src/org/etoile/animation/ode/controller/SimbiconWalkingController.java
+++ OdeApp/src/org/etoile/animation/ode/controller/SimbiconWalkingController.java
@@ -21,107 +21,182 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package org.etoile.animation.ode.controller;
 
 import org.etoile.animation.ode.OdeHumanoid;
+import org.etoile.animation.ode.OdeRigidBody;
 
 /**
  *
- * @author Jing Huang  * <gabriel.jing.huang@gmail.com or jing.huang@telecom-paristech.fr>
+ * @author Jing Huang * <gabriel.jing.huang@gmail.com or
+ * jing.huang@telecom-paristech.fr>
  */
 public class SimbiconWalkingController implements BaseController {
+
     String _name = "BalanceController";
     int _index;
     boolean _active = true;
     JSimbiconController _controller = new JSimbiconController();
     boolean _lostControl = false;
     double Md, Mdd;
-    
+    double DesVel = 0;
+    //this is the number of states that the biped needs in order to be fully characterized:  - 7*2 for each link + 2*(root translation/vel)
+    public final static int nrStates = 18;
+
+    //this is the state of the biped
+    public double State[] = new double[nrStates];
+    //and we'll keep a copy of the default biped state, so that we can restart from it
+    public double CopyState[] = new double[nrStates];
+    //and this is the array of monitor point positions
+    public double MonState[] = new double[50];
+
+    OdeHumanoid _human;
+    OdeRigidBody _leftFoot;
+    OdeRigidBody _rightFoot;
+
     public SimbiconWalkingController(OdeHumanoid human) {
+        _human = human;
         _controller.addWalkingController();
         _controller.addRunningController();
         _controller.addCrouchWalkController();
+
+        _leftFoot = _human.getBody("foot_l");
+        _rightFoot = _human.getBody("foot_r");
     }
-    
+
     @Override
     public void update(double dt) {
-        int body=0, stanceHip, swingHip;
+        int body = 0, stanceHip, swingHip;
         double fallAngle = 60;
         double torq[] = new double[7];
-        for (int n=0; n<7; n++)
-                torq[n] = 0;
+        for (int n = 0; n < 7; n++) {
+            torq[n] = 0;
+        }
 
-            // The following applies the control FSM.
-            // As part of this, it computes the virtual fb torque for the
-            // body, as implemented by a simple PD controller wrt to the world up vector
-            if (_lostControl)  
-                bip7WalkFsm(torq, dt);
+        // The following applies the control FSM.
+        // As part of this, it computes the virtual fb torque for the
+        // body, as implemented by a simple PD controller wrt to the world up vector
+        if (_lostControl) {
+            bip7WalkFsm(torq, dt);
+        }
 
         // now change torq[body], which is virtual, 
         // to include a FEL feed-forward component
+        // compute stance leg torque based upon body and swing leg
+        if (_controller.state[_controller.fsmState].leftStance) {
+            stanceHip = 3;   // left hip
+            swingHip = 1;   // right hip
+        } else {
+            stanceHip = 1;   // right hip
+            swingHip = 3;   // left hip
+        }
 
-          // compute stance leg torque based upon body and swing leg
-            if (_controller.state[_controller.fsmState].leftStance) {
-                    stanceHip = 3;   // left hip
-                    swingHip  = 1;   // right hip
-            } else {
-                    stanceHip = 1;   // right hip
-                    swingHip  = 3;   // left hip
-            }
+        if (!_controller.state[_controller.fsmState].poseStance) {
+            torq[stanceHip] = -torq[body] - torq[swingHip];
+        }
+        torq[0] = 0;         // no external torque allowed !
 
-            if (!_controller.state[_controller.fsmState].poseStance) {
-                    torq[stanceHip] = -torq[body] - torq[swingHip];
-            }
-            torq[0] = 0;         // no external torque allowed !
-
-            for (int n=1; n<7; n++) {  
-                  torq[n] = boundRange(torq[n], _controller.torqLimit[0][n], _controller.torqLimit[1][n]);   // torq limits
-                  jointLimit(torq[n],n);		                                     // apply joint limits
-            }
+        for (int n = 1; n < 7; n++) {
+            torq[n] = boundRange(torq[n], _controller.torqLimit[0][n], _controller.torqLimit[1][n]);   // torq limits
+            jointLimit(torq[n], n);		                                     // apply joint limits
+        }
     }
-    
-    
+
     //////////////////////////////////////////////////////////
     //	PROC:	bip7WalkFsm(torq)
     //	DOES:	walking control FSM
     //////////////////////////////////////////////////////////
-    public void bip7WalkFsm(double torq[], double dt){
-            int torsoIndex  = 0;
-            int rhipIndex   = 1;
-            int rkneeIndex  = 2;
-            int lhipIndex   = 3;
-            int lkneeIndex  = 4;
-            int rankleIndex = 5;
-            int lankleIndex = 6;
-            boolean worldFrame[] = {
-                    false,  // torso
-                    true,   // rhip
-                    false,  // rknee
-                    true,   // lhip
-                    false,  // lknee
-                    false,  // rankle
-                    false   // lankle
-            };
+    public void bip7WalkFsm(double torq[], double dt) {
+        int torsoIndex = 0;
+        int rhipIndex = 1;
+        int rkneeIndex = 2;
+        int lhipIndex = 3;
+        int lkneeIndex = 4;
+        int rankleIndex = 5;
+        int lankleIndex = 6;
+        boolean worldFrame[] = {
+            false, // torso
+            true, // rhip
+            false, // rknee
+            true, // lhip
+            false, // lknee
+            false, // rankle
+            false // lankle
+        };
 
-            _controller.stateTime += dt;
-            ConState s = _controller.state[_controller.fsmState];
+        _controller.stateTime += dt;
+        ConState s = _controller.state[_controller.fsmState];
 
-            computeMdMdd();
-            for (int n=0; n<7; n++) {         // compute target angles for each joint
-                    double target  = s.th[n] + Md*s.thd[n] + Mdd*s.thdd[n];         // target state + fb actions
-                    target = boundRange(target, _controller.targetLimit[0][n], _controller.targetLimit[1][n]);    // limit range of target angle
-                    wPDtorq(torq, n, target, _controller.kp[n], _controller.kd[n], worldFrame[n]);  // compute torques
-            }
+        computeMdMdd();
+        for (int n = 0; n < 7; n++) {         // compute target angles for each joint
+            double target = s.th[n] + Md * s.thd[n] + Mdd * s.thdd[n];         // target state + fb actions
+            target = boundRange(target, _controller.targetLimit[0][n], _controller.targetLimit[1][n]);    // limit range of target angle
+            wPDtorq(torq, n, target, _controller.kp[n], _controller.kd[n], worldFrame[n]);  // compute torques
+        }
 
-            _controller.advance();   	// advance FSM to next state if needed
+        _controller.advance();   	// advance FSM to next state if needed
     }
-    
-     public void computeMdMdd(){
-        double stanceFootX = bip7.getStanceFootXPos(con);
-        Mdd = bip7.State[1] - DesVel;          // center-of-mass velocity error
-        Md = bip7.State[0] - stanceFootX;      // center-of-mass position error
-    }  
+
+    //////////////////////////////////////////////////////////
+    //  PROC: wPDtorq()
+    //  DOES: computes requires torque to move a joint wrt world frame
+    //////////////////////////////////////////////////////////
+    public void wPDtorq(double torq[], int joint, double dposn, double kp, double kd, boolean world) {
+        double joint_posn = State[4 + joint * 2];
+        double joint_vel = State[4 + joint * 2 + 1];
+        if (world) {                   // control wrt world frame? (virtual)
+            joint_posn += State[4];    // add body tilt
+            joint_vel += State[5];    // add body angular velocity
+        }
+        torq[joint] = kp * (dposn - joint_posn) - kd * joint_vel;
+    }
+
+    private void computeMdMdd() {
+        double stanceFootX = getStanceFootXPos(_controller);
+        Mdd = State[1] - DesVel;          // center-of-mass velocity error
+        Md = State[0] - stanceFootX;      // center-of-mass position error
+    }
+
+    private double getStanceFootXPos(JSimbiconController con) {
+        int iLHeel = 8;   // x-coord of left-heel monitor point
+        int iRHeel = 0;   // x-coord of right-heel monitor point
+        if (con.state[con.fsmState].leftStance) {
+            return MonState[iLHeel];
+        } else {
+            return MonState[iRHeel];
+        }
+    }
+
+    //////////////////////////////////////////////////////////
+    // PROC:  jointLimit()
+    // DOES:  enforces joint limits
+    //////////////////////////////////////////////////////////
+    public double jointLimit(double torq, int joint) {
+        double kpL = 800;
+        double kdL = 80;
+        double minAngle = _controller.jointLimit[0][joint];
+        double maxAngle = _controller.jointLimit[1][joint];
+        double currAngle = State[4 + joint * 2];
+        double currOmega = State[4 + joint * 2 + 1];
+
+        if (currAngle < minAngle) {
+            torq = kpL * (minAngle - currAngle) - kdL * currOmega;
+        } else if (currAngle > maxAngle) {
+            torq = kpL * (maxAngle - currAngle) - kdL * currOmega;
+        }
+        return torq;
+    }
+
+    private double boundRange(double value, double min, double max) {
+        if (value < min) {
+            value = min;
+        }
+
+        if (value > max) {
+            value = max;
+        }
+        return value;
+    }
 
     @Override
     public String getName() {
@@ -152,5 +227,5 @@ public class SimbiconWalkingController implements BaseController {
     public boolean isActive() {
         return _active;
     }
-    
+
 }
